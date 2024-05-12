08fb1c5e384825bbb6b18a7d56fb936b0532efb5
alex crabbe <alienzen@hotmail.com>
2024-05-12 14:32:50 +0000

    display addition information to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,28 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("you clicked submit");
             }else{
                 let gameType = this.getAttribute("data-type");
-                alert(`you clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
+
 })
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame () {
-    let num1 = math.ceil(Math.random()*25);
-    let num1 = math.ceil(Math.random()*25);
+function runGame (gameType) {
+    let num1 = Math.ceil(Math.random()*25);
+    let num2 = Math.ceil(Math.random()*25);
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    }else{
+        alert(`unkown game tpye: ${gameType}`);
+        throw `Unknown game type; ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer () {
@@ -38,8 +47,10 @@ function incrementWrongAnswer () {
 
 }
 
-function displayAdditionQuestion () {
-
+function displayAdditionQuestion (operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion () {
